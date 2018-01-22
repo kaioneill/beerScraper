6c45d2878c5a436866bc3a30c74bed1bd0acf24a
kaioneill <oneillkai@icloud.com>
2018-01-22 15:12:32 -0800

    tfidf improvements

--- beerTF.py
+++ beerTF.py
@@ -12,24 +12,24 @@ print('\n')
 print("loading beer info...")
 print('\n')
 
-scrape.getMainSoup(url)
-scrape.getDescriptions()
-names = scrape.getNames()
+# scrape.getMainSoup(url)
+# scrape.getDescriptions()
+# names = scrape.getNames()
+#
+#
+#
+#
+# with Pool(10) as p:
+# 	records = p.map(scrape.getSoup, scrape.beerLinks)
+#
+# descs = records
+#
+# for j in range(len(descs)):
+# 	descs[j] = descs[j].lower()
+# 	descs[j] = descs[j].replace(',','')
+# 	descs[j] = descs[j].replace('.','')
 
-
-
-
-with Pool(10) as p:
-	records = p.map(scrape.getSoup, scrape.beerLinks)
-
-descs = records
-
-for j in range(len(descs)):
-	descs[j] = descs[j].lower()
-	descs[j] = descs[j].replace(',','')
-	descs[j] = descs[j].replace('.','')
-
-#names = ['beer1', 'beer2']
+names = ['beer1', 'beer2']
 
 query = 'i like coffee and sweet beer'
 #query = input('what qualities do you look for in a beer?\n')
@@ -38,7 +38,7 @@ print('\n')
 #arr = [query]
 arr = []
 
-#descs = ['Pliny the Younger was Pliny the Elder’s nephew, in the case of this beer, the "Younger" is a triple IPA. Pliny the Younger is hopped three times more than our standard IPA, and is dry hopped four different times.', 'This beer is the real McCoy. Barrel aged and crammed with coffee, none other will stand in it’s way. Sought out for being delicious, it is notoriously difficult to track down. If you can find one, shoot to kill, because it is definitely wanted... dead or alive.']
+descs = ['Pliny the Younger was Pliny the Elder’s nephew, in the case of this beer, the "Younger" is a triple IPA. Pliny the Younger is hopped three times more than our standard IPA, and is dry hopped four different times.', 'This beer is the real McCoy. Barrel aged and crammed with coffee, none other will stand in it’s way. Sought out for being delicious, it is notoriously difficult to track down. If you can find one, shoot to kill, because it is definitely wanted... dead or alive.']
 
 
 for i in descs:
@@ -47,12 +47,37 @@ for i in descs:
     #print(arr)
 
 
-vectorizer = TfidfVectorizer(analyzer='word')
+vectorizer = TfidfVectorizer()
 tfidf = vectorizer.fit_transform(arr)
 
-print(vectorizer.transform([query]))
+# print(vectorizer.vocabulary_['pliny'])
+
+# print(vectorizer.get_feature_names()[27])
 
 
+
+for i in range(1, tfidf.shape[1]):
+	weight = tfidf[(1,i)]
+	if(weight < .1 and weight > 0.0):
+		print(weight)
+
+
+tempNums = vectorizer.transform([query]).toarray()
+tempList = []
+
+#print(tempNums)
+
+for i in range(len(tempNums)):
+    if tempNums[i].any() != 0:
+        tempList.append([i, tempNums[i]])
+
+
+#print(len(tempList))
+
+for i in range(len(tempList)):
+    temp = int(tempList[i][0])
+    #print(names[temp])
+    #print('\n')
 
 
 sim = tfidf * tfidf.T
