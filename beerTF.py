6f6477bd5c72e3807bbd71a109053ea3b1bb6134
kaioneill <oneillkai@icloud.com>
2018-01-22 16:29:51 -0800

    basic functionality

--- beerTF.py
+++ beerTF.py
@@ -29,16 +29,17 @@ for j in range(len(descs)):
 	descs[j] = descs[j].replace(',','')
 	descs[j] = descs[j].replace('.','')
 
-names = ['beer1', 'beer2']
+# names = ['beer1', 'beer2']
 
-query = 'i like coffee and sweet beer'
-#query = input('what qualities do you look for in a beer?\n')
-print('\n')
+# query = 'i like coffee and sweet beer'
+# query = input('what qualities do you look for in a beer?\n')
+# print('\n')
 
 #arr = [query]
 arr = []
 
 # descs = ['Pliny the Younger was Pliny the Elder’s nephew, in the case of this beer, the "Younger" is a triple IPA. Pliny the Younger is hopped three times more than our standard IPA, and is dry hopped four different times.', 'This beer is the real McCoy. Barrel aged and crammed with coffee, none other will stand in it’s way. Sought out for being delicious, it is notoriously difficult to track down. If you can find one, shoot to kill, because it is definitely wanted... dead or alive.']
+
 
 
 for i in descs:
@@ -55,69 +56,91 @@ tfidf = vectorizer.fit_transform(arr)
 
 
 clean = []
+newDescs = []
 
-for i in range(1, tfidf.shape[1]):
-	weight = tfidf[(1,i)]
-	if(weight < 0.05 and vectorizer.get_feature_names()[i] in descs[1]):
-		clean.append((i, weight))
 
+
+for j in range(len(descs)-1):
+	tempDesc = ''
+	for i in range(0, tfidf.shape[1]):
+		weight = tfidf[(j,i)]
+		tempName = vectorizer.get_feature_names()[i]
+		if(weight < .1 and weight > .07 and ((' ' + tempName + ' ') in descs[j])):
+			# print(tempName)
+			tempDesc += (tempName + ' ')
+			clean.append((i, tempName, weight))
+	newDescs.append(tempDesc)
 
 
 
 # print(tfidf.toarray())
-print(clean)
+# print(newDescs)
+descs = newDescs
 # print(vectorizer.get_feature_names()[165])
 
 
-tempNums = vectorizer.transform([query]).toarray()
-tempList = []
 
-#print(tempNums)
+wordCount = [0] * len(descs)
 
-for i in range(len(tempNums)):
-    if tempNums[i].any() != 0:
-        tempList.append([i, tempNums[i]])
+def typeStuff():
 
 
-#print(len(tempList))
+	userDesc = input("enter a few words to describe beer that you like\n")
+	userDesc = userDesc.lower()
+	userWords = userDesc.split(' ')
 
-for i in range(len(tempList)):
-    temp = int(tempList[i][0])
-    #print(names[temp])
-    #print('\n')
+	#print('\n')
 
 
-sim = tfidf * tfidf.T
-matches = sim.toarray()[0]
-
-#matches = matches[1:]
-
-#print(matches)
+	w = 0
+	for desc in descs:
+		for userWord in userWords:
+			for beerWord in desc:
+				if userWord == beerWord:
+					wordCount[w] += 1
+		w += 1
 
 
 
 
-# nameCount = []
-#
-# for i in range(len(names)):
-#     temp = [matches[i], names[i]]
-#     nameCount.append(temp)
-#
-#
-# nameCount = sorted(nameCount, key=itemgetter(0), reverse=True)
-#
-# #print(nameCount)
-#
-#
-#
-# q = False
-# for item in nameCount:
-#     if item[0] != 0:
-#         print(item[1])
-#         q = True
-#     else: break
-#
-# if not q: print("no matches, sorry")
+	print('\n')
 
 
-print('\n')
+
+	names = scrape.getNames()
+
+
+
+	nameCount = []
+
+	for i in range(len(names)-1):
+		temp = [wordCount[i], names[i]]
+		nameCount.append(temp)
+
+
+	nameCount = sorted(nameCount, key=itemgetter(0), reverse=True)
+
+	#print(nameCount)
+
+	q = False
+	for item in nameCount:
+		if item[0] != 0:		#if count isn't zero
+			print(item[1])		#print the name
+			q = True
+		else: break
+
+	if not q: print("no matches, sorry")
+
+
+	print('\n')
+	#print(wordCount)
+	return
+typeStuff()
+
+
+
+
+while input('again? y/n\n') == 'y':
+	wordCount = [0] * len(descs)
+	names = []
+	typeStuff()
